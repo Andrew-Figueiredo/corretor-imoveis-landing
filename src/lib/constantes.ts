dcd7216be0f1fdb6b99d57c1796e5e7e494af1b1
Andrew-Figueiredo <andrewdw18@gmail.com>
2026-08-13 10:49:04 -0300

    chore: setup export estatico, tema visual e vitest

---
+++ src/lib/constantes.ts
@@ -0,0 +1,4 @@
+export const NOME_IMOBILIARIA = "Meu Cantinho Imóveis";
+export const CIDADE = "Vale das Acácias";
+export const NUMERO_WHATSAPP = "5511999999999";
+export const NOME_CORRETOR = "Carlos Andrade";
